188c89ea58fee869d1c9fa38af86adcf08364bdc
kewuaa <1692525710@qq.com>
2023-01-18 15:01:18 +0800

    update

---
+++ pytools/image/main.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from PIL import ImageTk, Image
+import tkinter as tk
+
+from ..lib.alib.aiofile import AWrapper
+from ..lib.alib import asynctk
+path = Path(__file__).parent
+
+
+async def __set(name: str, widget: tk.Widget, *widgets) -> None:
+    img = await aget(name)
+    # widget.update()
+    size = widget.winfo_width(), widget.winfo_height()
+    resized_img = img.resize(size, Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(resized_img)
+    widget.img = img
+    widget.configure(image=img)
+    if widgets:
+        for w in widgets:
+            w.configure(image=img)
+
+
+def set(name: str, widget: tk.Widget, *widgets) -> None:
+    widget.after(
+        10,
+        asynctk.create_task,
+        __set(name, widget, *widgets),
+    )
+
+
+async def aget(name: str) -> Image.Image:
+    img = tuple(path.glob(f'{name}.*'))
+    if not img:
+        raise RuntimeError(f'could not find img named {name} in {str(path)}')
+    img: Image.Image = await AWrapper(Image.open)(str(img[0]))
+    return img
+
+
+async def load(path: str or Path) -> Image.Image:
+    img = await AWrapper(Image.open)(str(path))
+    return img

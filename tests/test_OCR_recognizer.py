d6f6ab31b327312265302b964a6aebcf6f4c23f3
kewuaa <1692525710@qq.com>
2023-04-04 19:43:24 +0800

    update tests folder

--- tests/test_OCR_recognizer.py
+++ tests/test_OCR_recognizer.py
@@ -1,16 +1,11 @@
-from sys import path
 import asyncio
-path[0] = path[0] + '/..'
 
-from pytools.OCR_recognizer import Recognizer
+from pytools.OCR.recognizer import Recognizer
 recognizer = Recognizer()
 img_file = ''
 
 
-async def main():
-    await recognizer.recognize(img_file)
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+def test_recognizer():
+    async def main():
+        await recognizer.recognize(img_file)
+        asyncio.get_event_loop().run_until_complete(main())
--- tests/test_PDF_transformer.py
+++ tests/test_PDF_transformer.py
@@ -1,37 +1,32 @@
-from sys import path
 import asyncio
-path[0] = path[0] + '/..'
 
-from pytools.PDF_transformer import Transformer
+from pytools.PDF.transformer import Transformer
+loop = asyncio.get_running_loop()
 transformer = Transformer()
 pdf_file = r"C:\Users\kewuaa\Desktop"
 word_file = ''
 img_file = ''
 
 
-async def test_word2pdf():
-    await transformer.word2pdf(word_file)
+def test_word2pdf():
+    async def test_word2pdf():
+        await transformer.word2pdf(word_file)
+    loop.run_until_complete(test_word2pdf())
 
 
-async def test_pdf2word():
-    await transformer.pdf2word(pdf_file)
+def test_pdf2word():
+    async def test_pdf2word():
+        await transformer.pdf2word(pdf_file)
+    loop.run_until_complete(test_pdf2word())
 
 
-async def test_pdf2img():
-    await transformer.pdf2img(pdf_file)
+def test_pdf2img():
+    async def test_pdf2img():
+        await transformer.pdf2img(pdf_file)
+    loop.run_until_complete(test_pdf2img())
 
 
-async def test_img2pdf():
-    await transformer.img2pdf(img_file)
-
-
-async def main():
-    await test_word2pdf()
-    await test_pdf2word()
-    await test_pdf2img()
-    # await test_img2pdf()
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+def test_img2pdf():
+    async def test_img2pdf():
+        await transformer.img2pdf(img_file)
+    loop.run_until_complete(test_img2pdf())
